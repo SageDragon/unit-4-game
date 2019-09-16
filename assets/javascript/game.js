feca700c172d3dee11786e9bfadcfa703ac753d5
Sage Dragon <sagethunderdragon@gmail.com>
2019-09-16 14:14:20 -0400

    Created basic page design found images and pseudocoded JS file

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+//Create charater profiles, stats, and global vars
+//Load charater profiles into page
+//Create charater selection function into page
+//Function for hiding/unhiding unselected or dead charaters
+//Create function for attack algorithm for attack and HP
+//Make reset game function
+//Make you lose game function
+//Make you won game function
